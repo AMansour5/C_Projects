5785df81197f38cff31a646076136bdd56b938cb
محمد أسامة <134337793+THE-coding-NOOB@users.noreply.github.com>
2023-05-28 19:38:05 +0300

    ون بيس عمك

--- b 12.c
+++ b 12.c
@@ -1,40 +1,45 @@
+// This code is written in C programming language
 #include <stdio.h>
 #include <math.h>
 
 // Define the function f(x) that we want to find the root of
 double f(double x) {
-    return pow(x, 3) + 4.5 * pow(x, 2) - 0.19 * x - 0.14;
+    return x*x*x + 4.5*x*x - 0.19*x - 0.14;
 }
 
-// Define the bisection method to find the root of the function f(x) within a given interval [a, b]
-double bisection(double a, double b, int n) {
-    // Calculate the midpoint of the interval
-    double m = (a + b) / 2;
-    // Calculate the value of the function at the midpoint
-    double fm = f(m);
-    // Check if the function value at the midpoint is zero or if we have reached the maximum number of iterations
-    if (fm == 0 || n == 0) {
-        // If so, return the midpoint as the approximate root
-        return m;
-    } else if (f(a) * fm < 0) {
-        // If the sign of the function value at the midpoint is opposite to that at the left endpoint,
-        // then the root lies in the left half of the interval
-        return bisection(a, m, n - 1);
-    } else {
-        // Otherwise, the root lies in the right half of the interval
-        return bisection(m, b, n - 1);
-    }
-}
-
-// Define the main function
 int main() {
-    // Define the interval [a, b] and the maximum number of iterations n
-    double a = 0, b = 1;
-    int n = 30;
-    // Find the approximate root of the function f(x) within the interval [a, b] using the bisection method
-    double root = bisection(a, b, n);
-    // Print the approximate root
-    printf("Approximate root: %f\n", root);
-    // Return 0 to indicate successful execution of the program
+    // Define the initial values of a, b, and m
+    double a = 0.0, b = 1.0, m, fm, fa, fb;
+    int i;
+
+    // Loop through 30 iterations to find the root of f(x)
+    for (i = 1; i <= 30; i++) {
+        // Calculate the midpoint m between a and b
+        m = (a + b) / 2.0;
+        // Calculate the value of f(m)
+        fm = f(m);
+        // Calculate the value of f(a)
+        fa = f(a);
+        // Calculate the value of f(b)
+        fb = f(b);
+
+        // Print the values of a, b, m, and f(m) for the current iteration
+        printf("Iteration %d: a = %lf, b = %lf, m = %lf, f(m) = %lf\n", i, a, b, m, fm);
+
+        // Check if the value of f(m) is close enough to zero to consider it a root
+        if (fabs(fm) < 1e-6) {
+            // If f(m) is close enough to zero, print the value of m as the root and exit the loop
+            printf("Root found at x = %lf\n", m);
+            break;
+        } else if (fa * fm < 0.0) {
+            // If f(a) and f(m) have opposite signs, set b to m
+            b = m;
+        } else {
+            // If f(a) and f(m) have the same sign, set a to m
+            a = m;
+        }
+    }
+
+    // Return 0 to indicate successful completion of the program
     return 0;
 }
