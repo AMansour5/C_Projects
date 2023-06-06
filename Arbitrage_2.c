10a2349621dc249634c5cea4c65ca20949f8caa1
AMansour5 <120982142+AMansour5@users.noreply.github.com>
2023-06-06 17:44:22 +0300

    arbitrage

--- Arbitrage_2.c
+++ Arbitrage_2.c
@@ -1,68 +1,124 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdbool.h>
+#include <limits.h>
 
-#define MAX_N 20
-#define EPSILON 1e-9
+double map[21][21];
+int ans[40], alen;
 
-bool hasArbitrage(int n, double table[MAX_N][MAX_N]) {
-    double dp[MAX_N][MAX_N];
-    
-    // Initialize the base case
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            if (i == j) {
-                dp[i][j] = 1.0;
-            } else {
-                dp[i][j] = 0.0;
-            }
-        }
-    }
-    
-    // Dynamic programming
-    for (int k = 0; k < n; k++) {
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (dp[i][k] * table[k][j] > dp[i][j]) {
-                    dp[i][j] = dp[i][k] * table[k][j];
+typedef struct Arc
+{
+    int to, step;
+} Arc;
+
+int spfa(int st, int n)
+{
+    double dis[21][21];
+    int pre[21][21], used[21][21];
+    memset(dis, 0, sizeof(dis));
+    memset(used, 0, sizeof(used));
+    int i, j;
+    Arc tn, tmp;
+    tn.to = st, tn.step = 0;
+    struct
+    {
+        Arc data[2000];
+        int front, rear;
+    } Q;
+    Q.front = 0;
+    Q.rear = 0;
+    Q.data[Q.rear++] = tn;
+    dis[st][0] = 1;
+    while (Q.front != Q.rear)
+    {
+        tn = Q.data[Q.front++];
+        if (Q.front == 2000)
+            Q.front = 0;
+        used[tn.to][tn.step] = 0;
+        if (tn.step > n || tn.step >= alen)
+            continue;
+        for (i = 1; i <= n; i++)
+        {
+            if (dis[tn.to][tn.step] * map[tn.to][i] > dis[i][tn.step + 1])
+            {
+                dis[i][tn.step + 1] = dis[tn.to][tn.step] * map[tn.to][i];
+                tmp.to = i, tmp.step = tn.step + 1;
+                pre[tmp.to][tmp.step] = tn.to;
+                if (used[tmp.to][tmp.step] == 0)
+                {
+                    used[tmp.to][tmp.step] = 1;
+                    Q.data[Q.rear++] = tmp;
+                    if (Q.rear == 2000)
+                        Q.rear = 0;
                 }
             }
         }
     }
-    
-    // Check for arbitrage
-    for (int i = 0; i < n; i++) {
-        if (dp[i][i] > 1.0 + EPSILON) {
-            return true;
+    for (i = 1; i <= n; i++)
+    {
+        if (dis[st][i] > 1.01)
+        {
+            if (i < alen)
+            {
+                alen = i;
+                j = st;
+                while (i >= 0)
+                {
+                    ans[i] = j;
+                    j = pre[j][i];
+                    i--;
+                }
+            }
+            return 0;
         }
     }
-    
-    return false;
+    return -1;
 }
 
-int main() {
-    int n;
-    
-    while (scanf("%d", &n) == 1) {
-        double table[MAX_N][MAX_N];
-        
-        // Read the conversion table
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i == j) {
-                    table[i][j] = 1.0;
-                } else {
-                    scanf("%lf", &table[i][j]);
+int main()
+{
+    int n, i, j;
+    FILE *arbitrage_file;
+    arbitrage_file = fopen("arbitrage.txt", "r");
+    if (arbitrage_file != NULL)
+    {
+        while (fscanf(arbitrage_file, "%d", &n) == 1)
+        {
+            for (i = 1; i <= n; i++)
+            {
+                for (j = 1; j <= n; j++)
+                {
+                    if (i == j)
+                        continue;
+                    fscanf(arbitrage_file, "%lf", &map[i][j]);
                 }
+                map[i][i] = 1;
+            }
+            alen = INT_MAX;
+            for (i = 1; i <= n; i++)
+            {
+                spfa(i, n);
+            }
+            if (alen != INT_MAX)
+            {
+                for (i = 0; i <= alen; i++)
+                {
+                    if (i)
+                        putchar(' ');
+                    printf("%d", ans[i]);
+                }
+                puts("");
+            }
+            else
+            {
+                puts("no arbitrage sequence exists");
             }
         }
-        
-        // Check if arbitrage exists
-        if (hasArbitrage(n, table)) {
-            printf("Arbitrage sequence exists\n");
-        } else {_
-            printf("No arbitrage sequence exists\n");
-        }
     }
-    
+    else
+    {
+        printf("Not able to open the file.\n");
+    }
+    fclose(arbitrage_file);
     return 0;
 }
